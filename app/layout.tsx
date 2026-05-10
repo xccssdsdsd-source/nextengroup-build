e66e8a9b9b882d0dde7d8c0480de3efa6252661d
hvrs321412 <kajetan.kosinski1@gmail.com>
2026-05-10 21:24:28 +0200

    dds

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,6 +91,8 @@ export default function RootLayout({
     >
       <head>
         <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KVGVGL8M');`}} />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6D0PC33PCQ"></script>
+        <script dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-6D0PC33PCQ');`}} />
       </head>
       <body className="bg-[#020810] text-[#e8f0ff] antialiased">
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KVGVGL8M" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>

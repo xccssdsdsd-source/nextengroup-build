9d892af446b704e9713204386048d553ff559304
hvrs321412 <kajetan.kosinski1@gmail.com>
2026-05-10 21:08:36 +0200

    dsd

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,7 +89,11 @@ export default function RootLayout({
       lang="pl"
       className={`${syne.variable} ${figtree.variable} ${barlowCondensed.variable}`}
     >
+      <head>
+        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KVGVGL8M');`}} />
+      </head>
       <body className="bg-[#020810] text-[#e8f0ff] antialiased">
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KVGVGL8M" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
         <Cursor />
         {children}
       </body>

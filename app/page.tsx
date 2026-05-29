a08b5c2e9e7b0f58c751b404640daa02ac691713
hvrs321412 <kajetan.kosinski1@gmail.com>
2026-05-29 13:39:38 +0200

    fh

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const Contact = dynamic(() => import('@/components/Contact'))
 const Footer = dynamic(() => import('@/components/Footer'))
 export default function Home() {
   return (
-    <main className="overflow-x-hidden">
+    <main>
       <Nav />
       <Hero />
       <Ticker />
